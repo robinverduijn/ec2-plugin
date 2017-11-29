e800c11679cd1b9742431e2a8a7c5d8a90eb5b08
Alex Varju <alex@varju.ca>
2017-11-28 17:41:10 -0800

    Add optional bootDelay parameter for SSH slaves

--- src/main/java/hudson/plugins/ec2/EC2AbstractSlave.java
+++ src/main/java/hudson/plugins/ec2/EC2AbstractSlave.java
@@ -166,7 +166,7 @@ public abstract class EC2AbstractSlave extends Slave {
         }
 
         if (amiType == null) {
-            amiType = new UnixData(rootCommandPrefix, slaveCommandPrefix, slaveCommandSuffix, Integer.toString(sshPort));
+            amiType = new UnixData(rootCommandPrefix, slaveCommandPrefix, slaveCommandSuffix, Integer.toString(sshPort), null);
         }
 
         return this;
@@ -583,7 +583,7 @@ public abstract class EC2AbstractSlave extends Slave {
     }
 
     public int getBootDelay() {
-        return amiType.isWindows() ? ((WindowsData) amiType).getBootDelayInMillis() : 0;
+        return amiType.getBootDelayInMillis();
     }
 
     public static ListBoxModel fillZoneItems(AWSCredentialsProvider credentialsProvider, String region) {
--- src/main/java/hudson/plugins/ec2/SlaveTemplate.java
+++ src/main/java/hudson/plugins/ec2/SlaveTemplate.java
@@ -269,7 +269,7 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
             boolean usePrivateDnsName, String instanceCapStr, String iamInstanceProfile, boolean useEphemeralDevices,
             String launchTimeoutStr) {
         this(ami, zone, spotConfig, securityGroups, remoteFS, type, ebsOptimized, labelString, mode, description, initScript,
-                tmpDir, userData, numExecutors, remoteAdmin, new UnixData(rootCommandPrefix, slaveCommandPrefix, slaveCommandSuffix, sshPort),
+                tmpDir, userData, numExecutors, remoteAdmin, new UnixData(rootCommandPrefix, slaveCommandPrefix, slaveCommandSuffix, sshPort, null),
                 jvmopts, stopOnTerminate, subnetId, tags, idleTerminationMinutes, usePrivateDnsName, instanceCapStr, iamInstanceProfile,
                 useEphemeralDevices, false, launchTimeoutStr, false, null);
     }
@@ -1092,7 +1092,7 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
         }
 
         if (amiType == null) {
-            amiType = new UnixData(rootCommandPrefix, slaveCommandPrefix, slaveCommandSuffix, sshPort);
+            amiType = new UnixData(rootCommandPrefix, slaveCommandPrefix, slaveCommandSuffix, sshPort, null);
         }
         return this;
     }
